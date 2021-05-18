2f5a38c67b6c4b255984cff895c632b0769abc03
Lucas Dalbonio <dalbonio2@gmail.com>
2021-05-17 22:19:04 -0300

    Adding a complexity layer to the client struct

--- graphql-rust/src/graphql/graphql_schema.rs
+++ graphql-rust/src/graphql/graphql_schema.rs
@@ -2,39 +2,49 @@ use juniper::{EmptyMutation, RootNode, EmptySubscription, GraphQLObject};
 use juniper::FieldResult;
 
 #[derive(GraphQLObject)]
-#[graphql(description = "A simple test user with name and id")]
-struct Member {
+#[graphql(description = "A client of the websystem")]
+struct Client {
     id: i32,
+    address: Address,
     name: String,
+}
+
+#[derive(GraphQLObject)]
+#[graphql(description = "A simple address")]
+struct Address {
+    street: String,
+    number: String,
+    city: String,
 }
 
 pub struct QueryRoot;
 
 #[juniper::graphql_object]
 impl QueryRoot {
-    fn members() -> FieldResult<Vec<Member>> {
+
+    fn version() -> FieldResult<String> {
+        Ok( String::from("0.0.1"))
+    }
+
+    fn clients() -> FieldResult<Vec<Client>> {
         Ok(vec![
-            Member {
+            Client {
                 id: 1,
+                address: Address{street: String::from("Street1"), number: String::from("30"), city: String::from("City1")},
                 name: "User1".to_owned(),
             },
-            Member {
+            Client {
                 id: 2,
+                address: Address{street: String::from("Street2"), number: String::from("30"), city: String::from("City2")},
                 name: "User2".to_owned(),
             }
         ])
     }
 
-    fn human(_id: i32) -> FieldResult<Member> {
-        Ok(Member {
-            id: 1,
-            name: "User1".to_owned(),
-        })
-    }
-
-    fn member(_id: i32) -> FieldResult<Member> {
-        Ok(Member {
+    fn client(_id: i32) -> FieldResult<Client> {
+        Ok(Client {
             id: 2,
+            address: Address{street: String::from("Street2"), number: String::from("30"), city: String::from("City2")},
             name: "User2".to_owned(),
         })
     }
